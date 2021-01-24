d05746aeb4dba0245590615124a0d40a04b8f041
Success Go <success.go@gaodacheng.com>
2021-01-24 22:20:52 +0800

    add

---
+++ Basic-Part-One/02-Sum/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+
+        int x = 74, y = 36;
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
