abaa9e41465055a36cc5f53ec0357425b8af2222
Success Go <success.go@gaodacheng.com>
2021-01-25 17:03:11 +0800

    2 个笑脸

--- Basic-Part-One/016-Print-Face/PrintFace.java
+++ Basic-Part-One/016-Print-Face/PrintFace.java
@@ -1,9 +1,9 @@
 public class PrintFace {
     public static void main(String[] args) {
-        System.out.println(" +\"\"\"\"\"+ ");
-        System.out.println("[| o o |]");
-        System.out.println(" |  ^  | ");
-        System.out.println(" | '-' | ");
-        System.out.println(" +-----+ ");
+        System.out.println(" +\"\"\"\"\"+  +\"\"\"\"\"+ ");
+        System.out.println("[| o o |][| o o |]");
+        System.out.println(" |  ^  |  |  ^  | ");
+        System.out.println(" | '-' |  | '-' | ");
+        System.out.println(" +-----+  +-----+ ");
     }
 }
