82c003d19110fb847f65c45b270149b941dd5bf1
Success Go <success.go@gaodacheng.com>
2021-01-25 16:05:33 +0800

    计算圆的周长和面积

---
+++ Basic-Part-One/011-Compute-Circle/ComputeCircle.java
@@ -0,0 +1,11 @@
+public class ComputeCircle {
+    public static void main(String[] args) {
+        double r = 7.5;
+
+        double perimeter = Math.PI * 2 * r;
+        double area = Math.PI * r * r;
+
+        System.out.println("Perimeter is: " + perimeter);
+        System.out.println("Area is: " + area);
+    }
+}
