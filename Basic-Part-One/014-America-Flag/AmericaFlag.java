7eacc0086b05538c07a0a87b55e503b4496d7d04
Success Go <success.go@gaodacheng.com>
2021-01-25 16:26:06 +0800

    打印美国旗

---
+++ Basic-Part-One/014-America-Flag/AmericaFlag.java
@@ -0,0 +1,23 @@
+public class AmericaFlag {
+    public static void main(String[] args) {
+        String a = "* * * * * * ==================================";
+        String b = " * * * * *  ==================================";
+        String c = "==============================================";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+    }
+}
